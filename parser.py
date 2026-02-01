a7515bc111981544374abde9be3092bdf0e7e55d
madhav joshi <150261093+madha-v@users.noreply.github.com>
2026-02-01 13:21:40 +0530

    Update parser.py

--- parser.py
+++ parser.py
@@ -11,12 +11,14 @@ def new_temp():
     temp_count += 1
     return t
 
+# ---------------- PROGRAM ----------------
+
 def p_program(p):
     '''program : program statement
                | statement'''
     pass
 
-# ---------- STATEMENTS ------------
+# ---------------- STATEMENTS ----------------
 
 def p_statement_decl(p):
     'statement : INT ID SEMI'
@@ -29,14 +31,26 @@ def p_statement_decl_assign(p):
 
 def p_statement_assign(p):
     'statement : ID ASSIGN expression SEMI'
+    if p[1] not in symbol_table:
+        print(f"Semantic Error: {p[1]} not declared")
     tac.append(f"{p[1]} = {p[3]}")
 
-# -------- EXPRESSIONS ------------
+def p_statement_print(p):
+    'statement : PRINT LPAREN ID RPAREN SEMI'
+    tac.append(f"print {p[3]}")
+
+# ---------------- EXPRESSIONS ----------------
 
 def p_expression_plus(p):
     'expression : expression PLUS term'
     t = new_temp()
     tac.append(f"{t} = {p[1]} + {p[3]}")
+    p[0] = t
+
+def p_expression_minus(p):
+    'expression : expression MINUS term'
+    t = new_temp()
+    tac.append(f"{t} = {p[1]} - {p[3]}")
     p[0] = t
 
 def p_expression_term(p):
@@ -49,17 +63,33 @@ def p_term_times(p):
     tac.append(f"{t} = {p[1]} * {p[3]}")
     p[0] = t
 
+def p_term_div(p):
+    'term : term DIVIDE factor'
+    t = new_temp()
+    tac.append(f"{t} = {p[1]} / {p[3]}")
+    p[0] = t
+
 def p_term_factor(p):
     'term : factor'
     p[0] = p[1]
 
-def p_factor_number(p):
+# ---------------- FACTOR ----------------
+
+def p_factor_num(p):
     'factor : NUMBER'
     p[0] = p[1]
 
 def p_factor_id(p):
     'factor : ID'
+    if p[1] not in symbol_table:
+        print(f"Semantic Error: {p[1]} not declared")
     p[0] = p[1]
+
+def p_factor_group(p):
+    'factor : LPAREN expression RPAREN'
+    p[0] = p[2]
+
+# ---------------- ERROR ----------------
 
 def p_error(p):
     print("Syntax Error")
